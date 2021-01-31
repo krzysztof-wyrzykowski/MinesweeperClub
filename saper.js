76d73d582aeb0add3fd36cbbc5885ade225685c2
Krzysztof Wyrzykowski <wyrzykowski92@gmail.com>
2021-01-31 15:53:37 +0100

    Methods getElementById changed to querySelector
    Every tile has class "tile" and dataset tileid.
    Minor improvements added and game runs more smoothly.

--- saper.js
+++ saper.js
@@ -10,6 +10,9 @@ let level = 1
 let boardsHTML = []
 let nrOfGameReports = 0;
 
+const gamePanel = document.querySelector('#gamePanel');
+const tilesDivs = gamePanel.getElementsByClassName('tile');
+
 class Tile {
     constructor(ID,neighbours) {
         this.ID = ID;
@@ -71,13 +74,12 @@ function drawBombs(boardSize) {
 }
 }
 function showBoard (levelToShow) {
-    gamePanel = document.querySelector('#gamePanel');
     gamePanel.innerHTML = boardsHTML[levelToShow-1]   
 }
 function generateBoard(boardSize) {
     let boardHTML = "";
     for(let i = 0;i<(boardSize*boardSize);i++) {
-            boardHTML += '<div class="tile" id="tile'+i+'"></div>';
+            boardHTML += `<div class="tile coveredTile" data-tileid ="${i}" ></div>`;
         if((i+1)%boardSize===0) {
             boardHTML += '<br>' ;   
         } 
@@ -89,13 +91,7 @@ function generateStandardBoards() {
     boardsHTML.push(generateBoard(20));
     boardsHTML.push(generateBoard(30));
 }
-function addTilesOnClick (boardSize) { 
-    for(let i = 0;i<(boardSize*boardSize);i++) { 
-        document.getElementById('tile'+i).addEventListener("click", () => {detonation(i);});
-        document.getElementById('tile'+i).addEventListener("contextmenu", () => {toggleFlag(i);});
-        document.getElementById('tile'+i).addEventListener('contextmenu', event => event.preventDefault());
-    }
-}
+
 function drawBoard (boardSize) {
     remainingToDetonate =  boardSize * boardSize - nrOfBomb;
     isAnyBombDetonated = false;
@@ -113,20 +109,21 @@ function drawBoard (boardSize) {
 function revealTile (tileID) {
     
     if(tiles[tileID].exposed===false) {
-        if (tiles[tileID].isBomb === true && isAnyBombDetonated === false) {
-                document.getElementById('tile'+tileID).style.backgroundImage = 'url("img/bombCrossedOut.png")';
-                document.getElementById('tile'+tileID).classList.add('detonatedTile');
-                document.getElementById('tile'+tileID).classList.remove('tile');
+
+        if (tiles[tileID].isBomb === true && isAnyBombDetonated === false) { // click on bomb
+                tilesDivs[tileID].style.backgroundImage = 'url("img/bombCrossedOut.png")';
+                tilesDivs[tileID].classList.add('detonatedTile');
+                tilesDivs[tileID].classList.remove('coveredTile');
                 tiles[tileID].exposed = true;
-        } else  if (tiles[tileID].isBomb === true && isAnyBombDetonated === true){
-                document.getElementById('tile'+tileID).style.backgroundImage = 'url("img/bomb.png")';
-                document.getElementById('tile'+tileID).classList.add('detonatedTile');
-                document.getElementById('tile'+tileID).classList.remove('tile');
+        } else  if (tiles[tileID].isBomb === true && isAnyBombDetonated === true){ // detonating rest bombs
+                tilesDivs[tileID].style.backgroundImage = 'url("img/bomb.png")';
+                tilesDivs[tileID].classList.add('detonatedTile');
+                tilesDivs[tileID].classList.remove('coveredTile');
                 tiles[tileID].exposed = true;
-        } else {
-                document.getElementById('tile'+tileID).style.backgroundImage = 'url("img/tile'+tiles[tileID].content+'.png")';
-                document.getElementById('tile'+tileID).classList.add("detonatedTile");
-                document.getElementById('tile'+tileID).classList.remove("tile");
+        } else { // no bomb
+                tilesDivs[tileID].style.backgroundImage = 'url("img/tile'+tiles[tileID].content+'.png")';
+                tilesDivs[tileID].classList.add("detonatedTile");
+                tilesDivs[tileID].classList.remove("coveredTile");
                 tiles[tileID].exposed = true;
                 remainingToDetonate -= 1;
         }
@@ -140,12 +137,12 @@ function revealTile (tileID) {
     }  
 }
 function detonation (tileID) {
-    if (isAnyBombDetonated === false && tiles[tileID].flagged === false && finished == false) {
+    if (isAnyBombDetonated === false && tiles[tileID].flagged === false && finished === false) {
 
         if (tiles[tileID].isBomb === true) {
 
             if (firstDetonated === true) {
-                //lose
+                // lose
                 revealTile(tileID);
                 isAnyBombDetonated = true; 
 
@@ -157,8 +154,7 @@ function detonation (tileID) {
                     if(el.isBomb === true) {
                         revealTile(i);    
                     } else if (el.flagged === true) {
-                        document.getElementById('tile'+i).style.backgroundImage = 'url("img/crossedFlag.png")';
-                        document.getElementById('tile'+i).classList.remove('tile');
+                        tilesDivs[i].style.backgroundImage = 'url("img/crossedFlag.png")';
                         incorrectlyFlagged++;
                     }    
                 });
@@ -186,14 +182,14 @@ function toggleFlag(tileID) {
     if (isAnyBombDetonated === false && tiles[tileID].exposed === false && finished === false) {
 
         if (tiles[tileID].flagged === false) {
-            document.getElementById('tile'+tileID).classList.add('flaggedTile');
-            document.getElementById('tile'+tileID).classList.remove('tile');
+            tilesDivs[tileID].classList.add('flaggedTile');
+            tilesDivs[tileID].classList.remove('coveredTile');
             tiles[tileID].flagged = true;
 
             flaggedTiles += 1;
         } else {
-            document.getElementById('tile'+tileID).classList.add('tile');
-            document.getElementById('tile'+tileID).classList.remove('flaggedTile');
+            tilesDivs[tileID].classList.add('coveredTile');
+            tilesDivs[tileID].classList.remove('flaggedTile');
             
             tiles[tileID].flagged = false;
 
@@ -217,17 +213,17 @@ function refreshBombCounter () {
 function restart() {
     detonatedTiles = document.querySelectorAll(".detonatedTile");
 
-    detonatedTiles.forEach(el => {
+    detonatedTiles.forEach( el => {
         el.classList.remove('detonatedTile');
-        el.classList.add('tile');
+        el.classList.add('coveredTile');
         el.style.removeProperty('background-image');
     });
 
     flaggedTiles = document.querySelectorAll(".flaggedTile");
 
-    flaggedTiles.forEach(el => {
+    flaggedTiles.forEach( el => {
         el.classList.remove('flaggedTile');
-        el.classList.add('tile');
+        el.classList.add('coveredTile');
         el.style.removeProperty('background-image');
     });
 
@@ -239,7 +235,7 @@ function restart() {
 }
 function changeLevel(currentLevel) {
     document.querySelector('#face').style.backgroundImage = 'url("img/slightlySmilingFace.png")';
-    gamePanel = document.querySelector('#gamePanel');
+
     switch(currentLevel){
         case 1: {}
             size = 20;
@@ -250,8 +246,6 @@ function changeLevel(currentLevel) {
             gamePanel.style.setProperty("--tileBorder", "4px");
             showBoard(2);
             drawBoard(20);
-            addTilesOnClick(20);
-            refreshBombCounter();
         break;   
         case 2:
             size = 30;
@@ -262,8 +256,6 @@ function changeLevel(currentLevel) {
             gamePanel.style.setProperty("--tileBorder", "3px");
             showBoard(3);
             drawBoard(30);
-            addTilesOnClick(30);
-            refreshBombCounter();
         break;
         case 3:
             size = 10;
@@ -273,11 +265,11 @@ function changeLevel(currentLevel) {
             gamePanel.style.setProperty("--tileSize", "50px");
             gamePanel.style.setProperty("--tileBorder", "5px");
             showBoard(1);
-            drawBoard(10,10);
-            addTilesOnClick(10);
-            refreshBombCounter();
+            drawBoard(10,10);          
         break;
     }
+
+    refreshBombCounter();
 
     level += 1;
     if (level>3) {
@@ -360,9 +352,19 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#previousReport").addEventListener("click", function() {scrollReportsToLeft()});
     document.querySelector("#followingReport").addEventListener("click", function() {scrollReportsToRight()});
 
+    gamePanel.addEventListener("click", event => {
+        if(event.target.classList.contains("tile")) {
+           detonation(Number(event.target.dataset.tileid)); 
+        }
+        
+    });
+    gamePanel.addEventListener("contextmenu", event => {
+        event.preventDefault()
+        toggleFlag(Number(event.target.dataset.tileid))
+    });
+
     generateStandardBoards();
     showBoard(level);
     drawBoard(size);
     refreshBombCounter();
-    addTilesOnClick(size); 
 })
